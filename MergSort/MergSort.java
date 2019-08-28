95f6ebf3230636a8e0b394eecfe0131d17353369
Augusto Santos <augusto.santos@ccc.ufcg.edu.br>
2019-08-28 13:43:30 -0300

    Implementação MergeSort.

---
+++ MergSort/MergSort.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+public class MergSort {
+	
+	public static void main(String[] args) {	
+		int[] v = new int[] {2,5,4,3,6,8,10,-1};
+		mergeSort(v);
+		System.out.println(Arrays.toString(v));	
+	}
+	
+	public static void mergeSort(int[] v) {
+		mergeSort(v, 0, v.length-1);
+	}
+	
+	public static void mergeSort(int[] v, int inicio, int fim) {
+		int meio = (inicio + fim) / 2;
+		if (inicio < fim) {
+			mergeSort(v, inicio, meio);
+			mergeSort(v, meio+1, fim);					
+		}
+		merge(v, inicio, meio, fim);	
+	}
+	
+	private static void merge(int[] v, int inicio, int meio, int fim) {
+		int[] arrayAux = new int[v.length];
+		for (int i = 0; i < v.length; i++) {
+			arrayAux[i] = v[i];
+		}
+			
+		int i = inicio;
+		int j = meio + 1;
+		int k = inicio;
+		
+		while (i <= meio && j <= fim) {
+			if (arrayAux[i] < arrayAux[j]) {
+				v[k] = arrayAux[i];
+				i++;
+			} else {
+				v[k] = arrayAux[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while (i <= meio) {
+			v[k] = arrayAux[i];
+			i++;
+			k++;
+		}
+		
+		while (j <= fim) {
+			v[k] = arrayAux[j];
+			j++;
+			k++;
+		}
+		
+	}
+
+}
